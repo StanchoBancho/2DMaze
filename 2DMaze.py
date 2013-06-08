edee329393af181419805c9ae9c54ca21b5423bf
StanchoBancho <nikolov.stanimir@gmail.com>
2013-06-08 23:30:05 +0300

    Some progress with the World

--- 2DMaze.py
+++ 2DMaze.py
@@ -1,4 +1,5 @@
 import pygame
+import World
 
 class Game:
 
@@ -10,10 +11,15 @@ class Game:
     red = ( 255, 0, 0)
     running = True
 
-    def __init__(self, size):
+    def __init__(self, resolution):
         pygame.init()
-        self.screen = pygame.display.set_mode(size)
-        pygame.display.set_caption("2DMaze Game") 
+        self.screen = pygame.display.set_mode(resolution)
+        pygame.display.set_caption("2DMaze Game")
+
+        world_height = int(resolution[0] / 5) - 1 
+        world_widht = int(resolution[1] / 5) - 1
+        self.world = World.World((world_height, world_widht))
+    
 
     def main(self):
         clock = pygame.time.Clock()
@@ -35,6 +41,7 @@ class Game:
             self.screen.fill(self.white)
 
             #Draw everything
+            self.world.draw(self.screen)
             pygame.draw.rect(self.screen, self.green, (50,60,50,50))
 
             pygame.display.flip()
@@ -43,9 +50,3 @@ class Game:
 
 if __name__ == '__main__':
     Game((800, 600)).main()
-
-
-class Board:
-
-    def __init__(self, size):
-        pass
---
+++ World.py
@@ -0,0 +1,90 @@
+import pygame
+import random
+
+class World:
+
+    def __init__(self, size):
+
+        self.maze = Maze(size)
+        self.maze.make_random_puzzle()
+        
+    def draw(self, screen):
+        self.maze.draw(screen)              
+
+class Maze:
+
+    def __init__(self, size):
+        self.height = size[0]
+        self.width = size[1]
+        #-1 - unknown
+        #0  - empty
+        #1  - black
+        #2  - first player
+        #3  - second player
+        self.board = [[0 for j in range(self.height)] for i in range(self.width)]
+
+    def link(self, chosen_square, possible_moves):
+        i = chosen_square[0]
+        j = chosen_square[1]
+        links=[]
+        if i >= 3:
+            if self.board[i - 2][j] == -1:
+                self.board[i - 2][j] = 7
+                possible_moves.append((i - 2, j))
+            elif self.board[i - 2][j] == 1:
+                links.append((i - 1, j))
+                
+        if j + 3 <= self.height:
+            if self.board[i][j + 2] == -1:
+                self.board[i][j + 2] = 7
+                possible_moves.append((i, j + 2))
+            elif self.board[i][j + 2] == 1:
+                links.append((i , j + 1))
+
+        if j >= 3:
+            if self.board[i][j - 2] == -1:
+                self.board[i][j - 2] = 7
+                possible_moves.append((i , j - 2))
+            elif self.board[i][j - 2] == 1:
+                links.append((i , j - 1))
+
+        if i + 3 <= self.width:
+            if self.board[i + 2][j] == -1:
+                self.board[i + 2][j] = 7
+                possible_moves.append((i + 2, j));
+            elif self.board[i + 2][j] == 1:
+                links.append((i + 1 , j))
+
+        if len(links) > 0:
+            chosed_link = links[random.randrange(0, len(links))]
+            self.board[chosed_link[0]][chosed_link[1]] = 1
+            if chosed_link in possible_moves:
+                possible_moves.remove(chosed_link);
+	    
+    def make_random_puzzle(self):
+        #make the grid
+        for i in range(self.width):
+            for j in  range(self.height):
+                is_border_width = i == 0 or i == self.width - 1
+                is_border_height = j == 0 or j == self.height - 1
+                is_black = i % 2 == 1 or j % 2 == 1
+                if is_border_width or is_border_height:
+                    self.board[i][j] = 1
+                elif is_black:
+                    self.board[i][j] = -1
+                else:
+                    self.board[i][j] = 1
+        #make the puzzle
+        possible_moves = [(2, 2)]
+        while len(possible_moves) > 0:   
+            chosen_square = possible_moves[random.randrange(0, len(possible_moves))]
+            self.board[chosen_square[0]][chosen_square[1]] = 1
+            possible_moves.remove(chosen_square)
+            self.link(chosen_square, possible_moves)
+        
+        
+    def draw(self, screen):
+        for i in range(self.width):
+            for j in  range(self.height):
+                if self.board[i][j] == 1:
+                    pygame.draw.rect(screen, (0, 0, 0), (2 + j * 5, 2 + i * 5, 5, 5))
