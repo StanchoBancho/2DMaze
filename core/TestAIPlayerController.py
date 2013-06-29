0a201a2a6f6abf32491ba68d10aba9fa8556e579
StanchoBancho <nikolov.stanimir@gmail.com>
2013-06-29 10:41:41 +0300

    Some more unit tests added.

--- core/TestAIPlayerController.py
+++ core/TestAIPlayerController.py
@@ -12,12 +12,24 @@ class TestAIPlayerController(unittest.TestCase):
         self.world.add_player(self.player_one)
         tresure_coords = (self.world.width - 1, int(self.world.height/2))
         self.world.add_treasure(Treasure(self.world, tresure_coords))
-#        difficult = GameMenu.AI_NORMAL
-#        difficult = GameMenu.AI_NIGHTMARE
         difficult = GameMenu.AI_INFERNO
         self.player_controller = AIPlayerController(self.world,
                                                     self.player_one,
                                                     tresure_coords, difficult)
+
+    def test_create_heuristic_static_points(self):
+        good_known_positions = self.player_controller.known_good_positions
+        count = len(good_known_positions)
+        self.assertEqual(count, 200)
+        tresure_coords = (self.world.width - 1, int(self.world.height/2))
+        treasure_heuristic_val = good_known_positions[tresure_coords]
+        self.assertEqual(-200, treasure_heuristic_val)
+
+    def test_move_player(self):
+        cur_pos = self.player_one.position
+        self.player_controller.move_player()
+        real_pos = self.player_one.position
+        self.assertNotEqual(cur_pos, real_pos)
 
     def test_finding_game_treasure_movement(self):
         worst_case_turns = 1280 * 800
--- core/TestWorldObjects.py
+++ core/TestWorldObjects.py
@@ -11,6 +11,15 @@ class TestWorldObjects(unittest.TestCase):
         self.player_one = Player(self.world, (0, 1), "Stancho")
         self.world.add_player(self.player_one)
 
+    def test_add_player(self):
+        self.assertIn(self.player_one, self.world.players)
+
+    def test_add_treasure(self):
+        tresure_coords = (self.world.width - 1, int(self.world.height/2))
+        t = Treasure(self.world, tresure_coords)
+        self.world.add_treasure(t)
+        self.assertEqual(t, self.world.treasure)
+
     def test_player_movement(self):
         for i in range(10):
             for direction in Player.POSIBLE_DIRECTIONS:
@@ -22,3 +31,16 @@ class TestWorldObjects(unittest.TestCase):
                     self.assertEqual(self.player_one.position[0], new_x)
                     self.assertEqual(self.player_one.position[1], new_y)
                     break
+
+    def test_if_position_free(self):
+        position = (0, 0)  # part of the wall => black
+        result = self.world.is_position_free(position)
+        self.assertFalse(result)
+        result = self.world.is_position_free(self.player_one.position)
+        self.assertTrue(result)
+
+    def test_is_treasure_reached(self):
+        t = Treasure(self.world, self.player_one.position)
+        self.world.add_treasure(t)
+        result = self.world.is_treasure_reached()
+        self.assertTrue(result)
